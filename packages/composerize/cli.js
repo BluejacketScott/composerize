db9c16905d4d1eac41871c98c3a1ec72c94e225d
BluejacketScott <sr@bjs.onl>
2024-01-01 05:20:08 -0700

    Update cli.js
    modified to permit optional last param to be used as the name: <your project name> value; if not supplied then entire element is excluded from output

--- packages/composerize/cli.js
+++ packages/composerize/cli.js
@@ -3,16 +3,21 @@
 
 const composerize = require('./dist/composerize');
 
-const command = process.argv.slice(2).join(' ');
+const command = process.argv[2];
+const projectName = process.argv[3];
 
 if (process.stdin.isTTY){
-	console.log(composerize(command));
+    let output = composerize(command);
+    output = projectName ? output.replace('<your project name>', projectName) : output.replace('name: <your project name>\n', '');
+	console.log(output);
 }
 else {
 	var existingDockerCompose = '';
 	process.stdin.on('data', function(d) {
 		existingDockerCompose += d;
 	}).on('end', function() {
-		console.log(composerize(command, existingDockerCompose));
+		let output = composerize(command, existingDockerCompose);
+        output = projectName ? output.replace('<your project name>', projectName) : output.replace('name: <your project name>\n', '');
+		console.log(output);
 	}).setEncoding('utf8');
 }
